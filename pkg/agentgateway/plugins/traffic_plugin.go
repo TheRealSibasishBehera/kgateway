b9eb40b45ac1796e2a3eb16b9a5034a6ef2d87b3
Sibasish Behera <fangedhamster3114@gmail.com>
2025-10-20 03:31:30 +0530

    change failure mode handling
    Signed-off-by: Sibasish Behera <fangedhamster3114@gmail.com>

--- pkg/agentgateway/plugins/traffic_plugin.go
+++ pkg/agentgateway/plugins/traffic_plugin.go
@@ -20,6 +20,7 @@ import (
 	"k8s.io/apimachinery/pkg/api/meta"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/kube-openapi/pkg/validation/spec"
 	"k8s.io/utils/ptr"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	gwv1 "sigs.k8s.io/gateway-api/apis/v1"
@@ -822,17 +823,17 @@ func processRateLimitPolicy(ctx krt.HandlerContext, gatewayExtensions krt.Collec
 }
 
 func processExtProcPolicy(ctx krt.HandlerContext, gatewayExtensions krt.Collection[*v1alpha1.GatewayExtension], trafficPolicy *v1alpha1.TrafficPolicy, policyName string, policyTarget *api.PolicyTarget) ([]AgwPolicy, error) {
-	if trafficPolicy.Spec.ExtProc.ExtensionRef == nil {
-		logger.Debug("skipping extproc policy with no extensionRef (likely disable policy)",
-			"policy", trafficPolicy.Name)
-		return nil, nil
-	}
+	// TODO: reject trafficPolicy.Spec.ExtProc.Disable or ProcessingMode here ??
 
 	gwExt, err := lookupGatewayExtension(ctx, gatewayExtensions, *trafficPolicy.Spec.ExtProc.ExtensionRef, trafficPolicy.Namespace, v1alpha1.GatewayExtensionTypeExtProc)
 	if err != nil {
 		return nil, err
 	}
+
 	extProc := (*gwExt).Spec.ExtProc
+	if extProc == nil {
+		return nil, fmt.Errorf("extproc provider is missing from gateway extension %s/%s", gwExt.Namespace, gwExt.Namespace)
+	}
 
 	var extProcSvcTarget *api.BackendReference
 	if extProc.GrpcService != nil && extProc.GrpcService.BackendRef != nil {
@@ -856,13 +857,13 @@ func processExtProcPolicy(ctx krt.HandlerContext, gatewayExtensions krt.Collecti
 	}
 
 	if extProcSvcTarget == nil {
-		return nil, fmt.Errorf("extproc policy %s/%s missing grpcService.backendRef in GatewayExtension <place_holder>",
-			trafficPolicy.Namespace, trafficPolicy.Name)
+		return nil, fmt.Errorf("extproc policy %s/%s missing backendRef in gateway extension %s/%s",
+			trafficPolicy.Namespace, trafficPolicy.Name, (*gwExt).Namespace, (*gwExt).Name)
 	}
 
-	extProcPolicySpec := &api.PolicySpec_ExtProc{
-		Target:           extProcSvcTarget,
-		FailureModeAllow: extProc.FailOpen,
+	failureMode := api.PolicySpec_ExtProc_FAIL_CLOSED
+	if extProc.FailOpen {
+		failureMode = api.PolicySpec_ExtProc_FAIL_OPEN
 	}
 
 	extProcPolicy := &api.Policy{
@@ -870,19 +871,20 @@ func processExtProcPolicy(ctx krt.HandlerContext, gatewayExtensions krt.Collecti
 		Target: policyTarget,
 		Spec: &api.PolicySpec{
 			Kind: &api.PolicySpec_ExtProc_{
-				ExtProc: extProcPolicySpec,
+				ExtProc: &api.PolicySpec_ExtProc{
+					Target:      extProcSvcTarget,
+					FailureMode: failureMode,
+				},
 			},
 		},
 	}
+
 	logger.Debug("generated ExtProc policy",
 		"policy", trafficPolicy.Name,
 		"agentgateway_policy", extProcPolicy.Name,
 		"target", extProcSvcTarget)
-	return []AgwPolicy{
-		{
-			Policy: extProcPolicy,
-		},
-	}, nil
+
+	return []AgwPolicy{{Policy: extProcPolicy}}, nil
 }
 
 // processLocalRateLimitPolicy processes local rate limiting configuration
